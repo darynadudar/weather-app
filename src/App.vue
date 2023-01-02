d11f753db1a78cf445b40d9d9c6d64f5aeb57838
Daryna <d.dudar@mavenecommerce.com>
2023-01-02 17:33:21 +0200

    Create style directories and files

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <template>
-  <h1>Weather App</h1>
   <p>Your location: {{ ipInfo.city }}</p>
-  <div>
-<!--    <p v-if="currentWeather.temp_c">{{currentWeather.temp_c}}</p>-->
-  </div>
+  <currentWeather
+      :current-weather="currentWeatherObj"
+  />
 </template>
 
 <script>
+
 import Axios from "axios";
+import CurrentWeather from "./components/currentWeather.vue";
 
 export default {
   name: 'App',
+  components: {CurrentWeather},
   data() {
     return {
       API_KEY: 'a32967cbc7c04d1d982213031223012',
       ipInfo: {},
-      currentWeather: null,
+      currentWeatherObj: null,
     }
   },
 
@@ -36,7 +38,7 @@ export default {
     },
     getCurrentWeather() {
       Axios.get(`https://api.weatherapi.com/v1/current.json?key=${this.API_KEY}&q=${this.ipInfo.ip}`)
-          .then(response => this.currentWeather = response.data.current)
+          .then(response => this.currentWeatherObj = response.data.current)
     },
   },
 }
@@ -45,3 +47,6 @@ export default {
 <style>
 
 </style>
+<script setup>
+import './styles/style.scss';
+</script>
---
+++ src/components/currentWeather.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p v-if="currentWeather && currentWeather.temp_c">
+      {{ currentWeather.temp_c }}&#176;
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "currentWeather",
+  props: {
+    currentWeather: null,
+  },
+  data() {
+    return {
+      today: '',
+    }
+  },
+  mounted() {
+    this.getDate();
+  },
+  methods: {
+    getDate() {
+     let date = new Date();
+      this.today = date.toLocaleDateString('en-US', {
+        weekday: 'long',
+      });
+      // var dd = String(today.getDate()).padStart(2, '0');
+      // var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      // var yyyy = today.getFullYear();
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-family: 'Limelight', cursive;
+  font-size: 48px;
+}
+</style>
